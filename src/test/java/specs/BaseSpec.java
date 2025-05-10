19fc9e92261c00f8e61cd27234c189fcb1d6e401
vladislav-andriushchenko <vladitheboy@gmail.com>
2025-05-10 20:23:47 +0400

    added additional method to spec class

--- src/test/java/specs/BaseSpec.java
+++ src/test/java/specs/BaseSpec.java
@@ -6,7 +6,7 @@ import io.restassured.specification.ResponseSpecification;
 
 import static helpers.CustomAllureListener.withCustomTemplates;
 import static io.restassured.RestAssured.with;
-import static io.restassured.filter.log.LogDetail.*;
+import static io.restassured.filter.log.LogDetail.ALL;
 import static io.restassured.http.ContentType.JSON;
 
 public class BaseSpec {
@@ -24,4 +24,11 @@ public class BaseSpec {
     public static ResponseSpecification responseSpec = new ResponseSpecBuilder()
             .log(ALL)
             .build();
+
+    public static ResponseSpecification getResponseWithStatusCode(int statusCode) {
+        return new ResponseSpecBuilder()
+                .log(ALL)
+                .expectStatusCode(statusCode)
+                .build();
+    }
 }
--- src/test/java/steps/ReqresSteps.java
+++ src/test/java/steps/ReqresSteps.java
@@ -7,73 +7,66 @@ import models.UserListResponse;
 import models.UserModel;
 
 import static io.restassured.RestAssured.given;
+import static specs.BaseSpec.getResponseWithStatusCode;
 import static specs.BaseSpec.requestSpec;
-import static specs.BaseSpec.responseSpec;
 
 public class ReqresSteps {
 
     private final String REGISTER_URL = "/register";
     private final String USER_URL = "/users";
 
-
-
-    public Response registerUser(String body, Integer statusCode) {
+    public Response registerUser(String body, int statusCode) {
         return given(requestSpec)
                 .body(body)
                 .when()
                 .post(REGISTER_URL)
                 .then()
-                .statusCode(statusCode)
-                .spec(responseSpec)
+                .spec(getResponseWithStatusCode(statusCode))
                 .extract().response();
     }
 
     @Step("Update a user with id {userId}")
-    public UserModel updateUser(String body, Integer userId, Integer statusCode) {
+    public UserModel updateUser(String body, int userId, int statusCode) {
         return given(requestSpec)
                 .body(body)
                 .when()
                 .put(USER_URL + "/" + userId)
                 .then()
-                .statusCode(statusCode)
-                .spec(responseSpec)
+                .spec(getResponseWithStatusCode(statusCode))
                 .extract()
                 .as(UserModel.class);
     }
 
     @Step("Get single user with id {userId}")
-    public SingleUserResponse getUser(Integer userId, Integer statusCode) {
+    public SingleUserResponse getUser(int userId, int statusCode) {
         return given(requestSpec)
                 .when()
                 .get(USER_URL + "/" + userId)
                 .then()
-                .statusCode(statusCode)
-                .spec(responseSpec)
+                .spec(getResponseWithStatusCode(statusCode))
                 .extract()
                 .as(SingleUserResponse.class);
     }
 
     @Step("Get list of users from page {page}")
-    public UserListResponse getUsers(Integer page, Integer statusCode) {
+    public UserListResponse getUsers(int page, int statusCode) {
         return given(requestSpec)
                 .queryParam("page", page)
                 .when()
                 .get(USER_URL)
                 .then()
-                .statusCode(statusCode)
-                .spec(responseSpec)
+                .spec(getResponseWithStatusCode(statusCode))
                 .extract()
                 .as(UserListResponse.class);
     }
 
     @Step("Delete user with id {userId}")
-    public Response deleteUser(int userId, Integer statusCode) {
+    public Response deleteUser(int userId, int statusCode) {
         return given(requestSpec)
                 .when()
                 .delete(USER_URL + "/" + userId)
                 .then()
-                .statusCode(statusCode)
-                .spec(responseSpec)
+                .spec(getResponseWithStatusCode(statusCode))
                 .extract().response();
     }
 }
